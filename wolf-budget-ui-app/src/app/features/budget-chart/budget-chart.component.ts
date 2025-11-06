93031c800e01ba54e1fa40389832e1b7bd9cd15c
Sam <kondall.2@wright.edu>
2025-11-06 11:32:04 -0500

    com init

---
+++ wolf-budget-ui-app/src/app/features/budget-chart/budget-chart.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-chart',
+  imports: [],
+  templateUrl: './budget-chart.component.html',
+  styleUrl: './budget-chart.component.scss'
+})
+export class BudgetChartComponent {
+
+}
