69cc0f06b45c5bb94cec68a318ad61e1563dee81
honeypint <kclondon22@yahoo.com>
2025-11-11 16:20:12 -0500

    Render transaction-detail in transaction component

--- wolf-budget-ui-app/src/app/components/transaction-detail/transaction-detail.component.ts
+++ wolf-budget-ui-app/src/app/components/transaction-detail/transaction-detail.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, Input, signal } from '@angular/core'; 
 
 @Component({
   selector: 'app-transaction-detail',
   imports: [],
   templateUrl: './transaction-detail.component.html',
-  styleUrl: './transaction-detail.component.scss'
+  styleUrl: './transaction-detail.component.scss',
+  standalone: true
 })
 export class TransactionDetailComponent {
-
+  @Input({required: true}) transaction: {id: number, name: string} | null = null;
 }
--- wolf-budget-ui-app/src/app/components/transactions-page/transactions-page.component.ts
+++ wolf-budget-ui-app/src/app/components/transactions-page/transactions-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { TransactionService } from "../../services/transaction.service";
+import { TransactionService } from '../../services/transaction.service';
 import { FormsModule } from '@angular/forms'
+import { TransactionDetailComponent } from '../transaction-detail/transaction-detail.component'
 
 @Component({
   selector: 'app-transactions-page',
-  imports: [FormsModule],
+  imports: [FormsModule, TransactionDetailComponent],
   templateUrl: './transactions-page.component.html',
   styleUrl: './transactions-page.component.scss'
 })
