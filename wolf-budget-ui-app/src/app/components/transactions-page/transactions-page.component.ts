887eb5923456a694075dcd98f969a42fbddad7cd
honeypint <kclondon22@yahoo.com>
2025-11-11 16:09:25 -0500

    proper connection from service to component to add transactions

--- wolf-budget-ui-app/src/app/components/transactions-page/transactions-page.component.ts
+++ wolf-budget-ui-app/src/app/components/transactions-page/transactions-page.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
 import { TransactionService } from "../../services/transaction.service";
+import { FormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-transactions-page',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './transactions-page.component.html',
   styleUrl: './transactions-page.component.scss'
 })
@@ -13,4 +14,17 @@ export class TransactionsPageComponent {
   // inject TransactionService, grab transactions
   transactionService = inject(TransactionService);
   transactionSignal = this.transactionService.transactions;
+
+  // Input box Signal
+  newTransactionSignal = signal('');
+
+  createTransaction() {
+    // Grab the name from the textbox
+    const name = this.newTransactionSignal();
+    if (name == null) return;
+
+    // Create transaction sent to service, clear signal
+    this.transactionService.createTransaction(Date.now(), name);
+    this.newTransactionSignal.set('');
+  }
 }
