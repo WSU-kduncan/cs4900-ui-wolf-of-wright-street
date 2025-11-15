f56b89966d4acde8f60d53a366a41281411494ae
Daniel Cronauer <dcronaue@gmail.com>
2025-11-15 13:18:19 -0500

    Created interfaces for Transaction object to hold category and user, also cashflow type is inside of category

--- wolf-budget-ui-app/src/app/models/transaction.model.ts
+++ wolf-budget-ui-app/src/app/models/transaction.model.ts
@@ -1,4 +1,11 @@
+import { User } from './user.model';
+import { TransactionCategory } from './transactioncategory.model';
+
 export interface Transaction {
   id: number;
-  transactionName: string;
+  user: User;
+  category: TransactionCategory;
+  transactionDateTime: string; // ISO string from backend
+  description?: string; // optional
+  amount: number;
 }
---
+++ wolf-budget-ui-app/src/app/models/transactioncategory.model.ts
@@ -0,0 +1,7 @@
+import { CashflowType } from './cashflowtype.model';
+
+export interface TransactionCategory {
+  categoryName: string;
+  categoryDescription?: string;
+  cashflowType: CashflowType;
+}
