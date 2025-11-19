9504ecec7e5cfe0d039244af8e1015fd11e5683e
Daniel Cronauer <dcronaue@gmail.com>
2025-11-19 13:09:11 -0500

    tested app and now edit, delete, update works passing values correctly, also chose to move HTML to hidden input box, so value for ID can be referenced in EDIT or ignored in Create, without confusing the user

--- wolf-budget-ui-app/src/app/services/transaction.service.ts
+++ wolf-budget-ui-app/src/app/services/transaction.service.ts
@@ -11,13 +11,18 @@ export class TransactionService {
 
   createTransaction(newTransaction: Transaction): Observable<Transaction> {
     console.log('called create inside service');
-    this.transactions.update(trans => [...trans, newTransaction]);
+    //this.transactions.update(trans => [...trans, newTransaction]);
     console.log('past update');
     // post to back end
     return this.http.post<Transaction>(
     'http://localhost:8080/Wolf_of_Wright_Street_Service/transactions',
     newTransaction
-    );
+    ).pipe(
+    tap(created => {
+      // NOW update your state using the returned object WITH ID
+      this.transactions.update(trans => [...trans, created]);
+    })
+  );;
   }
 
   getTransactions(): Observable<Transaction[]> {
