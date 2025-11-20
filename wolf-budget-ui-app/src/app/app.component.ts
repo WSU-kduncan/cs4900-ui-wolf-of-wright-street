a0f8f435ef168bfe28e2f6a859048cfc9a5f7a17
Daniel Cronauer <dcronaue@gmail.com>
2025-11-20 07:46:26 -0500

    forget unsused import in my git add

--- wolf-budget-ui-app/src/app/app.component.ts
+++ wolf-budget-ui-app/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { EditTransactionComponent } from './components/edit-transaction/edit-transaction.component';
+//import { EditTransactionComponent } from './components/edit-transaction/edit-transaction.component';
 
 @Component({
   selector: 'app-root',
